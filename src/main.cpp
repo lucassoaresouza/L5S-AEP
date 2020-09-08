7abd886af3cf71cc7154732bbc54002e7db67a0a
= <lucas.soaresouza@gmail.com>
2020-09-08 09:23:06 -0300

    Compilando os tres modulos utilizando o cmake

--- src/main.cpp
+++ src/main.cpp
@@ -8,31 +8,30 @@
 
 #include "../compiler/interpreter.hpp"
 #include "../compiler/command.hpp"
-#include "../compiler/test.hpp"
+
 
 using namespace Engine;
 using namespace Compiler;
 using namespace std;
 
 int main(int, char**){
-    std::string game_name="AEP";
-    std::pair<int,int> window_size(1024,768);
+    // std::string game_name="AEP";
+    // std::pair<int,int> window_size(1024,768);
 
-    std::string object_name="teste";
-    std::pair<int, int> object_position(100, 100);
-    Game& game = Game::initialize(game_name, window_size);
+    // std::string object_name="teste";
+    // std::pair<int, int> object_position(100, 100);
+    // Game& game = Game::initialize(game_name, window_size);
 
-    GameObject* obj_1 = new GameObject(object_name, object_position);
-    obj_1->set_sprite("./assets/bots/B-25c.png");
-    game.add_object(obj_1);
-    game.load_objects();
+    // GameObject* obj_1 = new GameObject(object_name, object_position);
+    // obj_1->set_sprite("./assets/bots/B-25c.png");
+    // game.add_object(obj_1);
+    // game.load_objects();
     // game.run();
 
-    ola_mano();
-    // Interpreter i;
-    // int res = i.parse();
-    // cout << "Parser finalizado! res: " << res << endl;
-    // return res;
+    // return 0;
 
-    return 0;
+    Interpreter i;
+    int res = i.parse();
+    cout << "Parser finalizado! res: " << res << endl;
+    return res;
 }
