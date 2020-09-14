5854bec806f3da6f6949388e9acaa5019ddec27c
= <lucas.soaresouza@gmail.com>
2020-09-14 01:07:27 -0300

    Realizando um exemplo de input textual

--- engine/src/game.cpp
+++ engine/src/game.cpp
@@ -64,6 +64,10 @@ bool Game::init(){
         Log().print("Houve um problema ao inicializar o modulo de imagem da SDL2");
         success = false;
     }
+    if(TTF_Init() == -1){
+        Log().print("Houve um problema ao inicializar o modulo de textos da SDL2");
+        success = false;     
+    }
     return success;
 }
 
@@ -81,15 +85,96 @@ void Game::close(){
 void Game::run(){
     SDL_Event e;
     bool quit = false;
+    
+
+//PARTE TESTE TEXT INPUT--------------------------------------------------------------
+    bool renderText = false;
+    SDL_Color textColor = {0, 0, 0, 0xFF};
+    SDL_Texture* textTexture;
+    std::string inputText = "Some Text";
+    TTF_Font *gFont = NULL;
+    gFont = TTF_OpenFont( "./assets/fonts/lazy.ttf", 28);
+	if( gFont == NULL )
+	{
+        std::cout << "DEEEEEEEEEEEUUUUUUU RUIIIIIIIIIIIIIIIM" << std::endl;
+    }
+    SDL_Surface* textSurface = TTF_RenderText_Solid( gFont, inputText.c_str(), textColor );
+    current_texture = SDL_CreateTextureFromSurface(renderer, textSurface);
+
+    SDL_StartTextInput();
+//------------------------------------------------------------------------------------
+
     while(!quit){
         while(SDL_PollEvent(&e) != 0){
             if(e.type == SDL_QUIT){
                 quit = true;
             }
+//PARTE TESTE TEXT INPUT--------------------------------------------------------------
+            //Special key input
+            else if( e.type == SDL_KEYDOWN )
+            {
+                //Handle backspace
+                if( e.key.keysym.sym == SDLK_BACKSPACE && inputText.length() > 0 )
+                {
+                    //lop off character
+                    std::cout << "HAHAHAHA" << std::endl;
+                    inputText.pop_back();
+                    renderText = true;
+                }
+                //Handle copy
+                else if( e.key.keysym.sym == SDLK_c && SDL_GetModState() & KMOD_CTRL )
+                {
+                    SDL_SetClipboardText( inputText.c_str() );
+                }
+                //Handle paste
+                else if( e.key.keysym.sym == SDLK_v && SDL_GetModState() & KMOD_CTRL )
+                {
+                    inputText = SDL_GetClipboardText();
+                    renderText = true;
+                }
+            }
+            //Special text input event
+            else if( e.type == SDL_TEXTINPUT )
+            {
+                //Not copy or pasting
+                if( !( SDL_GetModState() & KMOD_CTRL && ( e.text.text[ 0 ] == 'c' || e.text.text[ 0 ] == 'C' || e.text.text[ 0 ] == 'v' || e.text.text[ 0 ] == 'V' ) ) )
+                {
+                    //Append character
+                    inputText += e.text.text;
+                    renderText = true;
+                }
+            }
         }
-        SDL_RenderClear(renderer);
+
+        //Rerender text if needed
+        if( renderText )
+        {
+            //Text is not empty
+            if( inputText != "" )
+            {
+                //Render new text
+                SDL_Surface* textSurface = TTF_RenderText_Solid( gFont, inputText.c_str(), textColor );
+                current_texture = SDL_CreateTextureFromSurface(renderer, textSurface);
+            }
+            //Text is empty
+            else
+            {
+                //Render space texture
+                std::string x = " ";
+                SDL_Surface* textSurface = TTF_RenderText_Solid( gFont, x.c_str(), textColor );
+                current_texture = SDL_CreateTextureFromSurface(renderer, textSurface);
+            }
+        }
+
+        //Clear screen
+        // SDL_SetRenderDrawColor( renderer, 0xFF, 0xFF, 0xFF, 0xFF );
+        SDL_RenderClear( renderer );
         SDL_RenderCopy(renderer, current_texture, NULL, NULL);
-        SDL_RenderPresent(renderer);
+        SDL_RenderPresent( renderer );
+//------------------------------------------------------------------------------------
+        // SDL_RenderClear(renderer);
+        // SDL_RenderCopy(renderer, current_texture, NULL, NULL);
+        // SDL_RenderPresent(renderer);
     }
     close();
 }
--- src/main.cpp
+++ src/main.cpp
@@ -17,25 +17,23 @@ using namespace Compiler;
 using namespace std;
 
 int main(int, char**){
-    // std::string game_name="AEP";
-    // std::pair<int,int> window_size(1024,768);
+    std::string game_name="AEP";
+    std::pair<int,int> window_size(1024,768);
 
-    // std::string object_name="teste";
-    // std::pair<int, int> object_position(100, 100);
-    // Game& game = Game::initialize(game_name, window_size);
+    std::string object_name="teste";
+    std::pair<int, int> object_position(100, 100);
+    Game& game = Game::initialize(game_name, window_size);
 
     // GameObject* obj_1 = new GameObject(object_name, object_position);
     // obj_1->set_sprite("./assets/bots/B-25c.png");
     // game.add_object(obj_1);
     // game.load_objects();
-    // game.run();
+    game.run();
 
-    // return 0;
-
-    AEPCompiler* compiler = new AEPCompiler();
-    std::string test = "";
-    cin >> test;
-    compiler->run(test);
+    // AEPCompiler* compiler = new AEPCompiler();
+    // std::string test = "";
+    // cin >> test;
+    // compiler->run(test);
 
     return 0;
 }
